c28075c591e7b082403e815b0238e44db9cae412
python-fifteen <61668178+python-fifteen@users.noreply.github.com>
2020-03-03 19:54:14 +0300

    Initial commit
    First version

---
+++ FifteenSound/FifteenSound.lua
@@ -0,0 +1,19 @@
+local ADDON = ...
+local PATH = "Interface\\AddOns\\FifteenSound\\SoundFiles\\"
+local LSM = LibStub("LibSharedMedia-3.0")
+
+--- LSM:Register("sound", "FifteenSound: XXXXXXXX", [[Interface\Addons\FifteenSound\ZZZZZZZZ\XXXXXXXX.ogg]])
+
+---------------------
+-- SOVA --
+---------------------
+--- LSM:Register("sound", "FifteenSound: XXXXXXXX", [[Interface\Addons\FifteenSound\SOVA\XXXXXXXX.ogg]])
+
+LSM:Register("sound", "FifteenSound: SOVA: Spread", [[Interface\Addons\FifteenSound\SOVA\spread.ogg]])
+
+-----------------------
+-- Custom --
+-----------------------
+--- LSM:Register("sound", "FifteenSound: Custom: XXXXXXXX", [[Interface\Addons\FifteenSound\Custom\XXXXXXXX.ogg]])
+
+LSM:Register("sound", "FifteenSound: Custom: Aiaiai", [[Interface\Addons\FifteenSound\Custom\Aiaiai.ogg]])
