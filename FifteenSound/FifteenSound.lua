83195792d4cdef8fb31a365702edea1c3699a16f
python-fifteen <61668178+python-fifteen@users.noreply.github.com>
2020-03-26 01:03:27 +0300

    New sound

--- FifteenSound/FifteenSound.lua
+++ FifteenSound/FifteenSound.lua
@@ -10,6 +10,9 @@ local LSM = LibStub("LibSharedMedia-3.0")
 --- LSM:Register("sound", "FifteenSound: XXXXXXXX", [[Interface\Addons\FifteenSound\SOVA\XXXXXXXX.ogg]])
 
 LSM:Register("sound", "FifteenSound: SOVA: Spread", [[Interface\Addons\FifteenSound\SOVA\spread.ogg]])
+LSM:Register("sound", "FifteenSound: SOVA: KICK", [[Interface\Addons\FifteenSound\SOVA\cast.ogg]])
+LSM:Register("sound", "FifteenSound: SOVA: U NEXT KICK", [[Interface\Addons\FifteenSound\SOVA\u_next_cast.ogg]])
+LSM:Register("sound", "FifteenSound: SOVA: Stone", [[Interface\Addons\FifteenSound\SOVA\warlock_noob.ogg]])
 
 -----------------------
 -- Custom --
